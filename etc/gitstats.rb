2b4fa50799e695e46c894dec426f4e646b7e84af
Ramkumar Ramachandra <artagnon@gmail.com>
2013-06-22 01:47:21 +0530

    etc/gitstats.rb: rewrite logic
    Use for-each-ref to get tags, instead of constructing by hand.

--- etc/gitstats.rb
+++ etc/gitstats.rb
@@ -1,23 +1,26 @@
+def num_author rev_range, pattern
+  num = Integer `git rev-list --count --author=\"#{pattern}\" #{rev_range}`
+  puts "#{rev_range}:: #{num}"
+end
+
+def sl_summary rev_range
+  sl_nums = `git shortlog -ns --no-merges #{rev_range} | cut -f 1 | head -n 10`.split("\n").map { |x| Integer x }
+  sl_names = `git shortlog -ns --no-merges #{rev_range} | cut -f 2 | head -n 10`.split("\n").map do |name|
+    name.split(" ", 2).map { |x| x[0].downcase }.join
+  end
+  sl_str = (sl_names.zip sl_nums).map { |zipped| "#{zipped[0]} (#{zipped[1]})" }.join(", ")
+  rl_str = `git rev-list --count #{rev_range}`
+  return if not rl_str.length > 0
+  puts "#{rev_range}:: #{rl_str} #{sl_str}"
+end
+
 def main
-  majors = [7, 8]
-  minors = (0..11).to_a
-  majors.each do |major|
-    minors.each do |minor|
-      if major == 8 and minor == 3
-        rev_range = "v1.#{major}.#{minor}..master"
-      else
-        next if not system("git rev-parse v1.#{major}.#{minor + 1} >/dev/null")
-        rev_range = "v1.#{major}.#{minor}..v1.#{major}.#{minor + 1}"
-      end
-      sl_nums = `git shortlog -ns --no-merges #{rev_range} | cut -f 1 | head -n 10`.split("\n").map { |x| Integer x }
-      sl_names = `git shortlog -ns --no-merges #{rev_range} | cut -f 2 | head -n 10`.split("\n").map do |name|
-        name.split(" ", 2).map { |x| x[0].downcase }.join
-      end
-      sl_str = (sl_names.zip sl_nums).map { |zipped| "#{zipped[0]} (#{zipped[1]})" }.join(", ")
-      rl_str = `git rev-list --count #{rev_range}`
-      next if not rl_str.length > 0
-      puts "#{rev_range}:: #{rl_str} #{sl_str}"
-    end
+  lower_bound = "v1.7.0"
+  tag_list = `git for-each-ref --sort=taggerdate --format="%(refname:short)" refs/tags`.split("\n")
+  major_versions = tag_list.select { |version| /^v\d+.\d+.\d+$/ =~ version }
+  (major_versions.drop_while { |x| x != lower_bound } + ["master"]).each_cons(2) do |pair|
+    rev_range = "#{pair[0]}..#{pair[1]}"
+    sl_summary rev_range
   end
 end
 
