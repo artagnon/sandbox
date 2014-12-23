750fea0aa5e404389646eaae88b741311a1dff40
Ramkumar Ramachandra <artagnon@gmail.com>
2014-12-23 18:00:10 -0500

    magically, all three implementations are working

--- etc/llbug/llbug.ml
+++ etc/llbug/llbug.ml
@@ -15,13 +15,16 @@ let double_type = Llvm.double_type context
 let () =
   let m = create_module (global_context ()) "simple_module" in
   let ee = create m in
+  let fn = define_function "scramble" (function_type i32_type [||]) m in
+  let b = builder_at_end (global_context ()) (entry_block fn) in
+  let add = const_int i32_type 24 in
+  ignore (build_ret add b);
   let fn = define_function "ooo1" (function_type i32_type [||]) m in
   let b = builder_at_end (global_context ()) (entry_block fn) in
   let add = const_int i32_type 42 in
   ignore (build_ret add b);
   ignore (dump_module m);
-  ignore (Llvm_bitwriter.write_bitcode_file m "/tmp/ooo1.bit");
 
   let cplusty = Foreign.funptr (void @-> returning int32_t) in
   let fh = get_function_address "ooo1" cplusty ee in
-  print_int (Int32.to_int (fh ()));
+  print_int (Int32.to_int (fh ())); print_newline ();
