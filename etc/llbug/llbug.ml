3393a9a8a3c0e53ad0032562a754de62a3c54ec5
Ramkumar Ramachandra <artagnon@gmail.com>
2014-12-23 18:49:53 -0500

    only ml is broken due to usage semantics

--- etc/llbug/llbug.ml
+++ etc/llbug/llbug.ml
@@ -15,16 +15,22 @@ let double_type = Llvm.double_type context
 let () =
   let m = create_module (global_context ()) "simple_module" in
   let ee = create m in
-  let fn = define_function "scramble" (function_type i32_type [||]) m in
+  let fn = define_function "scramble" (function_type i32_type [| i32_type;
+								 i32_type |]) m in
   let b = builder_at_end (global_context ()) (entry_block fn) in
-  let add = const_int i32_type 24 in
+  let add = build_add (param fn 0) (param fn 1) "sum" b in
   ignore (build_ret add b);
-  let fn = define_function "ooo1" (function_type i32_type [||]) m in
+
+  let cplusty = Foreign.funptr (int32_t @-> int32_t @-> returning int32_t) in
+  let fh = get_function_address "scramble" cplusty ee in
+  print_int (Int32.to_int (fh 4l 2l)); print_newline ();
+
+  let fn = define_function "ooo1" (function_type i32_type [| i32_type;
+							     i32_type |]) m in
   let b = builder_at_end (global_context ()) (entry_block fn) in
-  let add = const_int i32_type 42 in
+  let add = build_sub (param fn 0) (param fn 1) "diff" b in
   ignore (build_ret add b);
   ignore (dump_module m);
 
-  let cplusty = Foreign.funptr (void @-> returning int32_t) in
   let fh = get_function_address "ooo1" cplusty ee in
-  print_int (Int32.to_int (fh ())); print_newline ();
+  print_int (Int32.to_int (fh 4l 2l)); print_newline ();
